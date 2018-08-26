5be9caa2ced0da6d98f9545dae05f886feb20574
ming <772634613@qq.com>
2018-08-26 13:42:31 +0800

    包名修改

--- app/src/androidTest/java/com/example/arnavigationdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/arnavigationdemo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.suning.arnavigationdemo", appContext.getPackageName());
+        assertEquals("com.example.arnavigationdemo", appContext.getPackageName());
     }
 }
